b80dab5c5194e6909e3803b23bc44440915247ef
Dedit <dedit.dlutwama@gmail.com>
2019-11-18 23:59:12 -0500

    [x] First commit statement

---
+++ src/main/java/com/lordworth/tenalysys/TenalysysApplication.java
@@ -0,0 +1,13 @@
+package com.lordworth.tenalysys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenalysysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TenalysysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lordworth/tenalysys/TenalysysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lordworth.tenalysys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenalysysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
